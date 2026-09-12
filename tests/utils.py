2c8455414997297c6b7b7b55a0188b8c1b00fdbe
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-12 14:14:56 -0400

    RabeyaMuna__diff__id_229

--- tests/utils.py
+++ tests/utils.py
@@ -1,29 +1,29 @@
 from __future__ import annotations
 
+import contextlib
+from datetime import date, datetime
+import inspect
 import io
 import os
-import inspect
 import traceback
-import contextlib
-from typing import Any, TypeVar, Iterator, ForwardRef, Sequence, cast
-from datetime import date, datetime
-from typing_extensions import Literal, get_args, get_origin, assert_type
+from typing import Any, cast, ForwardRef, Iterator, Sequence, TypeVar
 
 import rich
+from typing_extensions import assert_type, get_args, get_origin, Literal
 
-from openai._types import Omit, NoneType
+from openai._models import BaseModel
+from openai._types import NoneType, Omit
+from openai._compat import field_outer_type, get_model_fields, PYDANTIC_V2
 from openai._utils import (
+    extract_type_arg,
+    is_annotated_type,
     is_dict,
     is_list,
     is_list_type,
-    is_union_type,
-    extract_type_arg,
     is_sequence_type,
-    is_annotated_type,
     is_type_alias_type,
+    is_union_type,
 )
-from openai._compat import PYDANTIC_V2, field_outer_type, get_model_fields
-from openai._models import BaseModel
 
 BaseModelT = TypeVar("BaseModelT", bound=BaseModel)
 
